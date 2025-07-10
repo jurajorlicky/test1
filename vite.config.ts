30cda5bb4d102aae6fed16b1a68c689248bafb23
juraj22 <56106259+jurajorlicky@users.noreply.github.com>
2025-07-10 16:09:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/test1/', // <-- názov repozitára, dôležité!
-  build: {
-    outDir: 'docs', // <-- build do docs, nie dist
-    target: 'esnext'
-  }
+  base: '/test1/',          // <- názov repozitára
+  build: { outDir: 'docs' } // <- build výstup do /docs
 })
